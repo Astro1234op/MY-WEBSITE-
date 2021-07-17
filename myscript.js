2a85b497fd5c9f236531774504ad5f51b6ef3855
Astro1234op <85688225+Astro1234op@users.noreply.github.com>
2021-07-17 17:47:44 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,9 @@
+function TurnOff(){
+  
+    document.getElementById('bulb').src='https://s3-projectsassets.whjr.online/Off+Bulb.png'
+    }
+    
+    function TurnOn(){
+      
+    document.getElementById('bulb').src='https://s3-projectsassets.whjr.online/On+Bulb.png'
+    }
